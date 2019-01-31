993a446feb436fc5b5efbf903d6a5b180a143301
justinavickers <justinav38@gmail.com>
2019-01-31 14:09:52 -0600

    used sort to find the highest and lowest grade

--- studentGrades/grades.js
+++ studentGrades/grades.js
@@ -1,24 +1,37 @@
-// Use console.info() to output the following criteria to the browser console.
+const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
+scores.sort(function(a, b){return a-b})
+console.log(scores[0])
+scores.sort(function(a, b){return b-a})
+console.log(scores[0])
+const grades = {
+  a:0,
+  b:0,
+  c:0,
+  d:0,
+  f:0
+}
 
-// How many of each grade? Accomplish this with a for..in loop.
-// What is the lowest score? Sort the array and find out.
-// What is the highest score?
+for (let i = 0; i < scores.length; i++) {
+  const grade = scores[i];
+  if (grade > 90) {
+    grades.a++
+  }
+  if(grade > 80 && grade < 90) {
+    grades.b++
+  }
+  if(grade > 70 && grade < 80) {
+    grades.c++
+  }
+  if(grade > 60 && grade < 70) {
+    grades.d++
+  }
+  if(grade > 50 && grade < 60) {
+    grades.f++
+  }
+}
+console.info(grades)
 // Which grade had the most students achieve it? Use an if statment,
 // and a currentGradeCount variable, in your for..in loop to see if the current
 // grade's value is higher than the last one.
 // Which grade had the fewest students achieve it?
 
-
-
-
-const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
-const grades = {} // You'll need to change this line of code
-
-for (let i = 0; i < scores.length; i++) {
-    /*
-      If the score is greater than 90, increment grades.A by 1.
-
-      You can use a series of `if/then` blocks, but you could
-      also achieve this with a switch statement.
-    */
-}
